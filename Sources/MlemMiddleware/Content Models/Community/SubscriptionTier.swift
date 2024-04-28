c2f91dc0dbf665cfaecd633e6e085fc385636772
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-04-28 18:38:01 -0400

    compiles

--- Sources/MlemMiddleware/Content Models/Community/SubscriptionTier.swift
+++ Sources/MlemMiddleware/Content Models/Community/SubscriptionTier.swift
@@ -9,37 +9,4 @@ import SwiftUI
 
 enum SubscriptionTier {
     case unsubscribed, subscribed, favorited
-    
-    var foregroundColor: Color {
-        switch self {
-        case .unsubscribed:
-            return .secondary
-        case .subscribed:
-            return .green
-        case .favorited:
-            return .blue
-        }
-    }
-    
-    var backgroundColor: Color {
-        switch self {
-        case .unsubscribed:
-            return .secondary
-        case .subscribed:
-            return .green
-        case .favorited:
-            return .clear
-        }
-    }
-    
-    var systemImage: String {
-        switch self {
-        case .unsubscribed:
-            return Icons.personFill
-        case .subscribed:
-            return Icons.subscribed
-        case .favorited:
-            return Icons.favoriteFill
-        }
-    }
 }
--- Sources/MlemMiddleware/Internal/ScoringOperation.swift
+++ Sources/MlemMiddleware/Internal/ScoringOperation.swift
@@ -43,31 +43,3 @@ extension ScoringOperation {
         }
     }
 }
-
-extension ScoringOperation: AssociatedColor {
-    var color: Color? {
-        switch self {
-        case .upvote: return Colors.upvoteColor
-        case .downvote: return Colors.downvoteColor
-        case .none: return nil
-        }
-    }
-}
-
-extension ScoringOperation: AssociatedIcon {
-    var iconName: String {
-        switch self {
-        case .upvote: return Icons.upvoteSquare
-        case .downvote: return Icons.downvoteSquare
-        case .none: return Icons.resetVoteSquare
-        }
-    }
-    
-    var iconNameFill: String {
-        switch self {
-        case .upvote: return Icons.upvoteSquareFill
-        case .downvote: return Icons.downvoteSquareFill
-        case .none: return Icons.resetVoteSquareFill
-        }
-    }
-}
