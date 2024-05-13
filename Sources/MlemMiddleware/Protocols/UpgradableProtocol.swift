985978ba87f9380c05d7d1f085cf8147d34f25c8
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-05-13 18:26:32 -0400

    tweaked Upgradable

--- Sources/MlemMiddleware/Protocols/UpgradableProtocol.swift
+++ Sources/MlemMiddleware/Protocols/UpgradableProtocol.swift
@@ -12,9 +12,8 @@ public protocol Upgradable {
     /// The canonical upgraded type for the entity conforming to this protocol
     associatedtype Upgraded
     
-    /// Whether this item is upgraded or not
-    /// Ideally Upgraded would be flagged as a protocol and we could do this generically with a "if let _ = self as? Upgraded" but I can't find a nice way to do that [ Eric 2024-05.13 ]
-    func isUpgraded() -> Bool
+    /// Upgraded type, if this entity is fully upgraded
+    var upgraded: Upgraded? { get }
     
     /// Upgrade this item and return the upgraded type
     func upgrade() async throws -> Upgraded
