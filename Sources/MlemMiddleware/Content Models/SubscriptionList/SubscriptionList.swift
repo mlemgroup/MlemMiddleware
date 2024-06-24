3fd86d208ccc4787da4647c4fc8c7fd6b97f638f
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-06-24 11:15:45 +0100

    Update

--- Sources/MlemMiddleware/Content Models/SubscriptionList/SubscriptionList.swift
+++ Sources/MlemMiddleware/Content Models/SubscriptionList/SubscriptionList.swift
@@ -13,6 +13,7 @@ public class SubscriptionList {
     public private(set) var communities: Set<Community2> = .init()
     public private(set) var favorites: [Community2] = .init()
     public private(set) var alphabeticSections: [String?: [Community2]] = .init()
+    public private(set) var instanceSections: [String?: [Community2]] = .init()
     
     internal var favoriteIDs: Set<Int> {
         get { getFavorites() }
@@ -41,7 +42,7 @@ public class SubscriptionList {
         favoriteIDs.contains(community.id)
     }
     
-    private func categoryForCommunity(_ community: Community2) -> String? {
+    private func alphabeticCategoryForCommunity(_ community: Community2) -> String? {
         let first = String(community.name.first ?? "#").folding(options: .diacriticInsensitive, locale: .current)
         guard first.first?.isLetter ?? false else { return nil }
         return first.uppercased()
@@ -50,13 +51,36 @@ public class SubscriptionList {
     @MainActor
     internal func updateCommunities(with communities: Set<Community2>) {
         self.communities = communities
-        let sections: [String?: [Community2]] = .init(
+        
+        // Alphabetical
+        
+        var alphabeticSections: [String?: [Community2]] = .init()
+        
+        let alphabeticSectionsGrouping: [String?: [Community2]] = .init(
             grouping: communities,
-            by: { categoryForCommunity($0) }
+            by: { alphabeticCategoryForCommunity($0) }
         )
-        for section in sections {
-            self.alphabeticSections[section.key] = section.value.sorted(by: { $0.name < $1.name })
+        for section in alphabeticSectionsGrouping {
+            alphabeticSections[section.key] = section.value.sorted(by: { $0.name < $1.name })
         }
+        
+        self.alphabeticSections = alphabeticSections
+        
+        // Instance
+        
+        var otherSection = [Community2]()
+        let instanceSectionsGrouping: [String?: [Community2]] = .init(grouping: communities, by: \.host)
+        var instanceSections: [String?: [Community2]] = .init()
+        
+        for section in instanceSectionsGrouping {
+            if section.value.count == 1, let community = section.value.first {
+                otherSection.append(community)
+            } else {
+                instanceSections[section.key] = section.value.sorted(by: { $0.name < $1.name })
+            }
+        }
+        instanceSections[nil] = otherSection.sorted(by: { $0.name < $1.name })
+        self.instanceSections = instanceSections
     }
     
     func updateCommunitySubscription(community: Community2) {
@@ -79,12 +103,30 @@ public class SubscriptionList {
     }
         
     private func addCommunity(community: Community2) {
-        let category = self.categoryForCommunity(community)
         self.communities.insert(community)
-        if self.alphabeticSections.keys.contains(category) {
-            self.alphabeticSections[category]?.sortedInsert(community) { $0.name < community.name }
+        
+        let alphabeticCategory = self.alphabeticCategoryForCommunity(community)
+        if self.alphabeticSections.keys.contains(alphabeticCategory) {
+            self.alphabeticSections[alphabeticCategory]?.sortedInsert(community) { $0.name < community.name }
         } else {
-            self.alphabeticSections[category] = [community]
+            self.alphabeticSections[alphabeticCategory] = [community]
+        }
+        
+        let hostCategoryExists = instanceSections.keys.contains(community.host)
+        let hostExists: Bool = (
+            hostCategoryExists || instanceSections[nil, default: []].contains(where: { $0.host == community.host })
+        )
+        
+        if hostExists {
+            if hostCategoryExists {
+                instanceSections[community.host]?.sortedInsert(community) { $0.name < community.name }
+            } else {
+                if let otherCommunity = instanceSections[nil]?.removeFirst(where: { $0.host == community.host }) {
+                    instanceSections[community.host] = [community, otherCommunity].sorted { $0.name < $1.name }
+                } else {
+                    instanceSections[nil, default: []].append(community)
+                }
+            }
         }
     }
     
@@ -92,7 +134,21 @@ public class SubscriptionList {
         self.communities.remove(community)
         self.favoriteIDs.remove(community.id)
         favorites.removeFirst { $0 === community }
-        let category = self.categoryForCommunity(community)
+        let category = self.alphabeticCategoryForCommunity(community)
         self.alphabeticSections[category]?.removeFirst { $0 === community }
+        
+        if var items = instanceSections[community.host] {
+            switch items.count {
+            case 1:
+                instanceSections.removeValue(forKey: community.host)
+            case 2:
+                items.removeFirst { $0 === community }
+                instanceSections[nil, default: []].sortedInsert(items[0], for: { $0.name < community.name })
+            default:
+                instanceSections[community.host]?.removeFirst { $0 === community }
+            }
+        } else {
+            self.alphabeticSections[nil]?.removeFirst { $0 === community }
+        }
     }
 }
