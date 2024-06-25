7f6e8123be357b39fdcb5277825e817275dd8a79
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-06-25 21:53:26 +0100

    Update ApiClient+General.swift

--- Sources/MlemMiddleware/API Client/ApiClient+General.swift
+++ Sources/MlemMiddleware/API Client/ApiClient+General.swift
@@ -8,8 +8,21 @@
 import Foundation
 
 public extension ApiClient {
-    func resolve(actorId: URL) async throws -> ApiResolveObjectResponse {
+    func resolve(actorId: URL) async throws -> (any ActorIdentifiable)? {
         let request = ResolveObjectRequest(q: actorId.absoluteString)
-        return try await perform(request)
+        let response = try await perform(request)
+        if let post = response.post {
+            return caches.post2.getModel(api: self, from: post)
+        }
+        if let comment = response.comment {
+            return caches.comment2.getModel(api: self, from: comment)
+        }
+        if let person = response.person {
+            return caches.person2.getModel(api: self, from: person)
+        }
+        if let community = response.community {
+            return caches.community2.getModel(api: self, from: community)
+        }
+        throw ApiClientError.noEntityFound
     }
 }
