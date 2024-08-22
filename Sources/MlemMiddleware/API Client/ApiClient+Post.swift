5bba425eeb0823f15cfd8bc0895904fa3855668d
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-08-22 17:57:02 +0100

    Mark Inbox item as read on reply creation (#49)
    * Update
    
    * Bug fix
    
    * Update
    
    * Bug fix
    
    * Update
    
    * Tweak
    
    * Fix

--- Sources/MlemMiddleware/API Client/ApiClient+Post.swift
+++ Sources/MlemMiddleware/API Client/ApiClient+Post.swift
@@ -287,7 +287,9 @@ public extension ApiClient {
             formId: nil
         )
         let response = try await perform(request)
-        return caches.comment2.getModel(api: self, from: response.commentView)
+        let comment = caches.comment2.getModel(api: self, from: response.commentView)
+        comment.getCachedInboxReply()?.setKnownReadState(newValue: true)
+        return comment
     }
     
     func reportPost(id: Int, reason: String) async throws {
