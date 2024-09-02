f93b8bd6957c44d6fcf953c31a899b7e691151cd
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-09-02 12:41:28 -0400

    exploratory work

--- Sources/MlemMiddleware/API Client/ApiClient.swift
+++ Sources/MlemMiddleware/API Client/ApiClient.swift
@@ -131,7 +131,7 @@ public class ApiClient {
     func perform<Request: ApiRequest>(_ request: Request) async throws -> Request.Response {
         let urlRequest = try urlRequest(from: request)
         // this line intentionally left commented for convenient future debugging
-        // urlRequest.debug()
+        urlRequest.debug()
         let (data, response) = try await execute(urlRequest)
         if let response = response as? HTTPURLResponse {
             if response.statusCode >= 500 { // Error code for server being offline.
@@ -160,6 +160,7 @@ public class ApiClient {
     }
     
     private func execute(_ urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+        print("DEBUG executing...")
         // add 0.18x "auth" param if on 18.x instance and token defined
         if try await version < .v19_0, let token, let httpBody = urlRequest.httpBody {
             var body = JSON(httpBody)
