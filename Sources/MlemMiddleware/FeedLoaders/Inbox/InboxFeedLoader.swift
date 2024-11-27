d5f465c57f0a3f0e1f2da0b4214f4e27b5aa36f7
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-11-27 16:22:06 -0500

    stubbed in InboxFeedLoader

--- Sources/MlemMiddleware/FeedLoaders/Inbox/InboxFeedLoader.swift
+++ Sources/MlemMiddleware/FeedLoaders/Inbox/InboxFeedLoader.swift
@@ -5,19 +5,46 @@
 //  Created by Eric Andrews on 2024-11-26.
 //
 
-//enum InboxItem: FeedLoadable {
-//    case message(Message2)
-//    // case reply(Reply2)
-//}
+import Foundation
+
+enum InboxItem: FeedLoadable {
+    typealias FilterType = InboxItemFilterType
+    
+    case message(Message2)
+    case reply(Reply2)
+    
+    var baseValue: any FeedLoadable {
+        switch self {
+        case let .message(message2): message2
+        case let .reply(reply2): reply2
+        }
+    }
+    
+    var api: ApiClient { baseValue.api }
+    
+    func sortVal(sortType: FeedLoaderSort.SortType) -> FeedLoaderSort {
+        baseValue.sortVal(sortType: sortType)
+    }
+    
+    var actorId: URL {
+        baseValue.actorId
+    }
+}
 
 //class AnyInboxItem: FeedLoadable {
 //    
 //}
 
-//class InboxFetcher: MultiFetcher<InboxItem> {
-//    
-//}
-//
-//class InboxFeedLoader: StandardFeedLoader<InboxItem> {
-//    
-//}
+class InboxFetcher: MultiFetcher<InboxItem> {
+    
+}
+
+class InboxFeedLoader: StandardFeedLoader<InboxItem> {
+    init(api: ApiClient, pageSize: Int, sources: [any ChildFeedLoading], sortType: FeedLoaderSort.SortType) {
+        super.init(filter: .init(), fetcher: MultiFetcher(api: api, pageSize: pageSize, sources: sources, sortType: sortType))
+    }
+}
+
+class MyStupidClass {
+    var hi: Int
+}
