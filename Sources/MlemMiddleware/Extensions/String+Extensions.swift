199145d54b292c166779f9bc9dcf48a847d690ec
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-12-23 14:45:25 -0500

    doc

--- Sources/MlemMiddleware/Extensions/String+Extensions.swift
+++ Sources/MlemMiddleware/Extensions/String+Extensions.swift
@@ -8,15 +8,17 @@
 import Foundation
 
 public extension String {
+    /// Returns true if the given array of strings contains any word which appears as a substring of this string
     func isContainedIn(_ strings: [String]) -> Bool {
         strings.contains { contains($0) }
     }
     
+    /// Returns true if the given set of strings contains any word which appears as a substring of this string
     func isContainedIn(_ strings: Set<String>) -> Bool {
         strings.contains { contains($0) }
     }
     
-    /// Returns true if this string contains whole words that are in the given set of strings
+    /// Returns true if this string contains any whole word that is in the given set of strings
     func containsWordsIn(_ strings: Set<String>) -> Bool {
         let words = self.split(separator: " ").map { $0.lowercased() }
         return words.contains { strings.contains($0) }
