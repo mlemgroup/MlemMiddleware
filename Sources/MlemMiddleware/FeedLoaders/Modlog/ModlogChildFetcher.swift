99ec75ba3d867e791d9dbb5d01110a9b23c8370a
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-01-11 10:17:19 +0000

    Update

--- Sources/MlemMiddleware/FeedLoaders/Modlog/ModlogChildFetcher.swift
+++ Sources/MlemMiddleware/FeedLoaders/Modlog/ModlogChildFetcher.swift
@@ -10,14 +10,17 @@ import Foundation
 @Observable
 public class ModlogChildFetcher: Fetcher<ModlogEntry> {
     let sharedCache: SharedCache
+    var communityId: Int?
     var type: ApiModlogActionType
     
     init(
         api: ApiClient,
         pageSize: Int,
         sharedCache: SharedCache,
+        communityId: Int?,
         type: ApiModlogActionType
     ) {
+        self.communityId = communityId
         self.type = type
         self.sharedCache = sharedCache
         super.init(api: api, pageSize: pageSize)
@@ -28,7 +31,12 @@ public class ModlogChildFetcher: Fetcher<ModlogEntry> {
         if page == 1 {
             items = try await sharedCache.get(type: type)
         } else {
-            items = try await api.getModlog(page: page, limit: pageSize, type: type)
+            items = try await api.getModlog(
+                page: page,
+                limit: pageSize,
+                communityId: communityId,
+                type: type
+            )
         }
         
         return .init(
--- Sources/MlemMiddleware/FeedLoaders/Modlog/ModlogFeedLoader.swift
+++ Sources/MlemMiddleware/FeedLoaders/Modlog/ModlogFeedLoader.swift
@@ -10,12 +10,20 @@ import Foundation
 public class ModlogFeedLoader: StandardFeedLoader<ModlogEntry> {
     var modlogFetcher: MultiFetcher<ModlogEntry> { fetcher as! MultiFetcher }
     
+    var modlogSources: [ModlogChildFeedLoader] {
+        (modlogFetcher.sources as? [ModlogChildFeedLoader])!
+    }
+    
+    private var sharedCache: ModlogChildFetcher.SharedCache
+    
     public init(
         api: ApiClient,
         pageSize: Int,
+        communityId: Int?,
         sortType: FeedLoaderSort.SortType
     ) {
-        let sharedCache: ModlogChildFetcher.SharedCache = .init(api: api, pageSize: pageSize)
+        let sharedCache: ModlogChildFetcher.SharedCache = .init(api: api, pageSize: pageSize, communityId: communityId)
+        self.sharedCache = sharedCache
         
         let sources: [ModlogChildFeedLoader] = ApiModlogActionType.allFilteredCases.map { type in
                 .init(
@@ -25,6 +33,7 @@ public class ModlogFeedLoader: StandardFeedLoader<ModlogEntry> {
                         api: api,
                         pageSize: pageSize,
                         sharedCache: sharedCache,
+                        communityId: communityId,
                         type: type
                     )
                 )
@@ -37,5 +46,25 @@ public class ModlogFeedLoader: StandardFeedLoader<ModlogEntry> {
         for source in sources {
             source.setParent(parent: self)
         }
+    }
+    
+    public func items(ofType type: ApiModlogActionType) -> [ModlogEntry] {
+        modlogSources.first { $0.modlogFetcher.type == type }?.items ?? []
+    }
+    
+    public func refresh(
+        communityId: Int? = nil,
+        clearBeforeRefresh: Bool = false
+    ) async throws {
+        sharedCache.communityId = communityId
+        modlogSources.forEach { item in
+            item.modlogFetcher.communityId = communityId ?? item.modlogFetcher.communityId
+        }
+        try await refresh(clearBeforeRefresh: clearBeforeRefresh)
+    }
+    
+    override public func refresh(clearBeforeRefresh: Bool) async throws {
+        await sharedCache.reset()
+        try await super.refresh(clearBeforeRefresh: clearBeforeRefresh)
     }
 }
