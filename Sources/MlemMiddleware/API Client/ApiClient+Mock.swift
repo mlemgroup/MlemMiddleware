20b6174c24420d93da079b318e5e33ef589d8270
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-02-15 10:11:34 +0000

    update

--- Sources/MlemMiddleware/API Client/ApiClient+Mock.swift
+++ Sources/MlemMiddleware/API Client/ApiClient+Mock.swift
@@ -13,11 +13,29 @@ public extension ApiClient {
 }
 
 public class MockApiClient: ApiClient {
-    public init() {
+    var communities: [Community2]?
+    
+    public init(communities: [Community2]? = nil) {
+        self.communities = communities
         super.init(
             url: URL(string: "https://example.com/")!,
-            username: nil, permissions: .none
+            username: nil,
+            permissions: .all
         )
+    }
+    
+    override func perform<Request: ApiRequest>(_ request: Request, requiresToken: Bool = true) async throws -> Request.Response {
+        if let request = request as? SearchRequest, let communities {
+            return ApiSearchResponse(
+                type_: .all,
+                comments: [],
+                posts: [],
+                communities: communities.map(\.apiCommunityView),
+                users: []
+            ) as! Request.Response
+        }
+        
+        throw ApiClientError.insufficientPermissions
     }
 }
 #endif
