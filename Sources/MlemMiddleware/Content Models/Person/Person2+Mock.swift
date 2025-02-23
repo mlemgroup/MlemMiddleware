3b3f29b3d0aecd137154c50181fb0b3ef7d23ff5
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-02-23 18:36:13 +0000

    Update

--- Sources/MlemMiddleware/Content Models/Person/Person2+Mock.swift
+++ Sources/MlemMiddleware/Content Models/Person/Person2+Mock.swift
@@ -10,15 +10,13 @@ import Foundation
 #if DEBUG
 public extension Person2 {
     static func mock(
-        api: ApiClient = .mock,
         person1: Person1,
         postCount: Int,
         commentCount: Int,
         isAdmin: Bool
     ) -> Person2 {
-        assert(api === person1.api)
         return Person2(
-            api: api,
+            api: person1.api,
             person1: person1,
             postCount: postCount,
             commentCount: commentCount,
