095874d68a7a45ff11af28b014412c95128570e3
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-03-12 17:25:40 -0400

    add User-Agent (#134)
    * add User-Agent
    
    * mlemUrlRequest
    
    * mlemUrlRequest
    
    * Update MlemUrlRequest.swift
    
    * Update Post1+ImagePrefetchProviding.swift
    
    ---------
    
    Co-authored-by: Sjmarf <78750526+Sjmarf@users.noreply.github.com>

--- Sources/MlemMiddleware/Content Models/Post/Post1+ImagePrefetchProviding.swift
+++ Sources/MlemMiddleware/Content Models/Post/Post1+ImagePrefetchProviding.swift
@@ -20,24 +20,29 @@ extension Post1: ImagePrefetchProviding {
         switch type {
         case let .media(url), let .embedded(url, _):
             // media/embedded media: only load the media
+            var urlRequest: URLRequest
             switch config.imageSize {
             case .unlimited:
-                ret.append(ImageRequest(url: url, priority: .high))
+                urlRequest = mlemUrlRequest(url: url)
             case let .limited(size):
-                ret.append(ImageRequest(url: url.withIconSize(size), priority: .high))
+                urlRequest = mlemUrlRequest(url: url.withIconSize(size))
             }
+            ret.append(ImageRequest(urlRequest: urlRequest, priority: .high))
         case let .link(link):
             // websites: load image and favicon
             if config.fetchFavicons, let url = link.favicon {
-                ret.append(ImageRequest(url: url))
+                let urlRequest = mlemUrlRequest(url: url)
+                ret.append(ImageRequest(urlRequest: urlRequest))
             }
             if let url = link.thumbnail {
+                var urlRequest: URLRequest
                 switch config.imageSize {
                 case .unlimited:
-                    ret.append(ImageRequest(url: url, priority: .high))
+                    urlRequest = mlemUrlRequest(url: url)
                 case let .limited(size):
-                    ret.append(ImageRequest(url: url.withIconSize(size), priority: .high))
+                    urlRequest = mlemUrlRequest(url: url.withIconSize(size))
                 }
+                ret.append(ImageRequest(urlRequest: urlRequest, priority: .high))
             }
         default:
             break
